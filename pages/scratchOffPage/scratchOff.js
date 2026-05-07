8638ef7352fb8e81bc139c9869fa397d51b127c6
Alannah Steck <asteck6789@gmail.com>
2026-05-07 10:41:14 -0400

    random images and card flip not working :C

--- pages/scratchOffPage/scratchOff.js
+++ pages/scratchOffPage/scratchOff.js
@@ -1,7 +1,14 @@
 const scratchOffArea = document.getElementById("scratchOff");
 const winnersArea = document.getElementById("winners");
 
+function randNum(num){
+    const theIndex = Math.floor(Math.random() * r); 
+    return customersStorage[theIndex]
+}
+
 function genPage(){
+    const diff = sessionStorage.getItem("difficulty")
+    const imgs = [`../../resources/cells/${diff}/${diff}_beefcakeModulo.png`, `../../resources/cells/${diff}/${diff}_ferretGuy.png`, `../../resources/cells/${diff}/${diff}_negativeGuy.png`]
     const scratchOffBox = document.createElement("section");
     scratchOffBox.id = "scratchOffBox";
     scratchOffArea.appendChild(scratchOffBox);
@@ -11,7 +18,10 @@ function genPage(){
         scratchOffBox.appendChild(cellContainer); 
         for (let i=0; i<4; i++){
             const cell = document.createElement("div");
-            cell.className = "cell";
+            const theIndex = Math.floor(Math.random() * imgs.length); 
+            cell.style.backgroundImage = `url(${imgs[theIndex]})`
+            cell.classList.add("hidden", "cell");
+            cell.setAttribute("onclick",`scratch(this)`)
             cellContainer.appendChild(cell); 
         }
     }
@@ -19,4 +29,25 @@ function genPage(){
     const winBox = document.createElement("section");
     winBox.id = "winBox";
     winnersArea.appendChild(winBox);
+    const heading = document.createElement("h2");
+    heading.className = "accentText"
+    heading.textContent = "Winning Images";
+    winBox.appendChild(heading); 
+    for (let i=0; i<3; i++){
+        const cell = document.createElement("div");
+        cell.className = "cell";
+        winBox.appendChild(cell); 
+    }
+}
+
+
+function scratch(cell){
+    console.log("ONCLICK WORKS");
+    unhide(cell)
+}
+
+function unhide(cell){
+    console.log("REACHED");
+    item.classList.remove("hidden")
+    item.removeAttribute("onclick")
 }

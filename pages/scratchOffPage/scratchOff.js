5a22f2441e5b41cea412a4dd0f1074e11f5d2b17
Alannah Steck <asteck6789@gmail.com>
2026-05-07 08:47:01 -0400

    the cells are real

--- pages/scratchOffPage/scratchOff.js
+++ pages/scratchOffPage/scratchOff.js
@@ -1,6 +1,22 @@
-const scratchOffArea = document.getElementByidName("scratchOff");
-const winnersArea = document.getElementByidName("winners");
+const scratchOffArea = document.getElementById("scratchOff");
+const winnersArea = document.getElementById("winners");
 
 function genPage(){
-    pass
+    const scratchOffBox = document.createElement("section");
+    scratchOffBox.id = "scratchOffBox";
+    scratchOffArea.appendChild(scratchOffBox);
+    for (let i=0; i<4; i++){
+        const cellContainer = document.createElement("div");
+        cellContainer.className = "cellContainer";
+        scratchOffBox.appendChild(cellContainer); 
+        for (let i=0; i<4; i++){
+            const cell = document.createElement("div");
+            cell.className = "cell";
+            cellContainer.appendChild(cell); 
+        }
+    }
+
+    const winBox = document.createElement("section");
+    winBox.id = "winBox";
+    winnersArea.appendChild(winBox);
 }
